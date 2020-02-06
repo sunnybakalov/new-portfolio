192512084f19c4342efecda439d7501b69153e8d
Stan Bakalov <stan.bakalov@podium.com>
2020-02-06 13:53:02 -0700

    started on the cards that will display my projects

---
+++ src/components/ProjectCard/ProjectCard.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+export default function ProjectCard() {
+  return (
+    <div className="projectCard">
+      <div className="projectLinks">
+        
+      </div>
+      <div className="projectDescription">
+
+      </div>
+      <div className="projectTechnologies">
+
+      </div>
+    </div>
+  );
+};
--- src/screens/About/About.jsx
+++ src/screens/About/About.jsx
@@ -5,9 +5,25 @@ import '../../Main.css';
 const About = () => {
   return(
     <div className="container" id="about">
+      <h2>About Me</h2>
       <div className="aboutParagraph">
         <p>
-          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Saepe dicta eaque ipsum quaerat quasi ratione atque, nam architecto optio obcaecati accusantium placeat tempora quis aliquid. Nisi quaerat, magni totam rem in accusantium unde illum iste soluta error quisquam, consectetur sint nihil quo labore vel incidunt vitae eius ullam. Officiis, libero?
+          Hello! My name is Stan and I'm a software engineer based in Bluffdale, UT who enjoys front end development. I develop exceptional web apps as well as websites that provide user friendly interfaces with efficient, modern backends.
+        </p>
+        <br />
+        <p>
+          Shortly before I finished my bootcamp from the University of Utah, I joined the engineering team at Podium where I have learned more than I ever could imagine. My role consists of an even balance of frontend and backend work, and I've also gained some product management experience.
+        </p>
+        <br />
+        <p>
+          Here are some of the technologies that I've been working with:
+          <ul>
+            <li>React</li>
+            <li>JavaScript (ES6)</li>
+            <li>Elixir</li>
+            <li>Phoenix</li>
+            <li>Postgres</li>
+          </ul>
         </p>
       </div>
       <div className="portrait">
