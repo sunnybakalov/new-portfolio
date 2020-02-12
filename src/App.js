b39c0f4ac7e8ba1ceb6d9de066d3855563666b84
Stan Bakalov <stan.bakalov@podium.com>
2020-02-12 13:37:56 -0700

    dark mode toggle switch working

--- src/App.js
+++ src/App.js
@@ -1,64 +1,36 @@
 import React from 'react';
 import Fade from 'react-reveal/Fade';
-import Toggle from 'react-toggle'
 import './App.css';
 import Nav from '../src/components/Nav/Nav';
+import DarkMode from '../src/components/DarkMode/DarkMode';
 import Footer from '../src/components/Footer/Footer';
 import ScrollToTop from '../src/components/ScrollToTop/ScrollToTop';
-import DarkMode from '../src/components/DarkMode';
 import Home from '../src/screens/Home/Home';
 import About from '../src/screens/About/About';
 import Contact from '../src/screens/Contact/Contact';
 import Projects from '../src/screens/Projects/Projects';
 
-const body = document.querySelector('body');
-
-body.addEventListener('click', e => toggle(e));
-
-const toggle = event => {
-  if (body.classList.contains('light-mode')) {
-    body.classList.remove('light-mode');
-  } else {
-  body.classList.add('light-mode');  
-  }
-}
-
-export default class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      darkMode: true
-    }
-  }
-
-  handleToggleDarkMode = () => {
-    this.setState({
-      darkMode: !darkMode
-    })
-  }
-
-  render() {
-    return (
-      <div className="App">
-        <Nav />
-        <div className="main">
-          <DarkMode 
-            handleToggle={this.handleToggleDarkMode}
-          />
-          <Home id="home" />
-          <Fade>
-            <About id="about" />
-          </Fade>
-          <Fade>
-            <Projects id="projects" />
-          </Fade>
-          <Fade>
-            <Contact id="contact" />
-          </Fade>
-          <ScrollToTop className="scrollToTop"/>
-        </div>
-        <Footer />
+function App() {
+  return (
+    <div className="App">
+      <Nav />
+      <div className="main">
+        <DarkMode />
+        <Home id="home" />
+        <Fade>
+          <About id="about" />
+        </Fade>
+        <Fade>
+          <Projects id="projects" />
+        </Fade>
+        <Fade>
+          <Contact id="contact" />
+        </Fade>
+        <ScrollToTop className="scrollToTop"/>
       </div>
-    );
-  }
+      <Footer />
+    </div>
+  );
 }
+
+export default App;
--- src/components/DarkMode/DarkMode.jsx
+++ src/components/DarkMode/DarkMode.jsx
@@ -1,18 +1,37 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Toggle from 'react-toggle'
 import * as icon from '../../assets/images/index';
+import './darkmode.css';
 
-export const DarkMode = (props) => {
-  <label>
-    <Toggle
-      defaultChecked={props.darkMode}
-      icons={{
-        checked: icon.moon,
-        unchecked: icon.sun,
-      }}
-      onClick={props.handleToggle}
-      />
-    <span>Dark/Light Mode</span>
-  </label>
+const DarkMode = () => {
+  const [lightMode, setLightMode] = useState(false);
+
+  const body = document.querySelector('body');
+
+  const toggleMode = (event) => {
+    if (body.classList.contains('light-mode')) {
+      body.classList.remove('light-mode');
+      setLightMode(!lightMode);
+    } else {
+      body.classList.add('light-mode');
+      setLightMode(lightMode);
+    }
+  }
+
+  return (
+    <label className="modeToggle">
+      <Toggle
+        className="toggle"
+        defaultChecked={lightMode}
+        // icons={{
+        //   checked: icon.moon,
+        //   unchecked: icon.sun,
+        // }}
+        onClick={toggleMode}
+        />
+      <p>Dark/Light Mode</p>
+    </label>
+  );
 };
 
+export default DarkMode;
